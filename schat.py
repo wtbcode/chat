e6389217a5dfd9df1d14f46e49236826d7710b8c
Will Buttitta <will.buttitta@gmail.com>
2018-11-24 17:04:14 -0500

    Latest

--- schat.py
+++ schat.py
@@ -21,22 +21,32 @@ import sys
 import asyncio
 
 class ChatServerProtocol(asyncio.Protocol):
+    def __init__(self, connections):
+        self.connections = connections
+
     def connection_made(self, transport):
+        self.connections += [transport]
         self.transport = transport
 
-    def data_received(self, data):
-        print(data.decode())
+    def connection_lost(self, exc):
+        self.connections.remove(self.transport)
 
-async def main(host, port):
+    def data_received(self, data):
+        message = data.decode()
+        for connection in self.connections:
+            connection.write(data)
+
+async def main(host, port, connections):
     loop = asyncio.get_running_loop()
-    server = await loop.create_server(ChatServerProtocol, host, port)
+    server = await loop.create_server(lambda: ChatServerProtocol(connections), host, port)
     for socket in server.sockets:
         addr = socket.getsockname()
         print("Listening on {}:{}".format(addr[0], addr[1]))
 
     await server.serve_forever()
 
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 host = sys.argv[1] if len(sys.argv) > 1 else ""
 port = int(sys.argv[2]) if len(sys.argv) > 2 else 64000
-signal.signal(signal.SIGINT, signal.SIG_DFL)
-asyncio.run(main(host, port))
+connections = []
+asyncio.run(main(host, port, connections))
